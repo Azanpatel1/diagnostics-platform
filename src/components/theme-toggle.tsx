9bed3b0dcd06c209ea89d3389e3f59e3140c7a34
Azanpatel1 <104657925+Azanpatel1@users.noreply.github.com>
2026-02-01 14:50:27 -0800

    Restore system theme option

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -32,6 +32,10 @@ export function ThemeToggle() {
           <Moon className="mr-2 h-4 w-4" />
           Dark
         </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <span className="mr-2">💻</span>
+          System
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
